58b668b730be56d00c3a8e27c7a05dae2412783d
Yana Kryshchuk <kryshchuk@ucu.edu.ua>
2019-05-24 22:06:11 +0300

    Initial commit

---
+++ btnode.py
@@ -0,0 +1,10 @@
+class Node:
+    """ Representation of node."""
+
+    def __init__(self, item, left = None, right = None):
+        """
+        Initialization.
+        """
+        self.item = item
+        self.left = left
+        self.right = right
